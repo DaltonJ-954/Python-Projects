83c4f7653688b4022787ff02ea2a940ec1312392
Dalton Walls <scallywig@gmail.com>
2023-01-19 17:35:06 -0500

    push update

---
+++ OOB.py
@@ -0,0 +1,25 @@
+
+class Game:
+    variable1 = 'Hello'
+    variable2 = 'To you!'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    x = Game()
+    print("{} {}".format(x.variable1,x.variable2))
--- movies.py
+++ movies.py
@@ -1,35 +1,39 @@
-class movies: # This "movies" class is the parent
-    duration = ''
-    genre = ''
-    year = ''
+class Movies: # This "movies" class is the parent
+    duration = "120 min"
+    genre = 'horror'
+    film_year = '2015'
 
 
-class studios(movies): # The "studios" class is one that inherits the parent one
-    number_of_films = ''
-    budgets = ''
-    earnings = ''
-    company = ''
-
-
-
-class director(movies): # 2nd child class
-    movies_directed = ''
-    coDirected = ''
-    school = ''
-    highest_nominated = ''
+class studios(Movies): # The "studios" class is one that inherits the parent one
+    number_of_films = 600
+    budgets = '20 million'
+    earnings = '2.3 million'
+    company = 'Lions Gate'
 
 
 
-class actor(movies): # And this is the third class that inherits the parent
-    starred_in = ''
-    coStarre_in = ''
-    awards = ''
-    years_acting = ''
+class director(Movies): # 2nd child class
+    movies_directed = '26'
+    coDirected = '4 films'
+    school = 'Duke'
+    highest_nominated_award = 'Grammy'
+
+
+
+class actor(Movies): # And this is the third class that inherits the parent
+    starred_in = 0
+    coStarred_in = 0
+    awards = 0
+    years_acting = 0
 
 # All classes each have one or more attributes to them
 
 
+x = director()
 
-print(actor)
+if __name__ == "__main__":
+    Movies()
+    print("Mel went to " + x.school + " college.")
+    print("Mel directed " x.movies_directed " films for " + x.company + " personally.")
     
     
