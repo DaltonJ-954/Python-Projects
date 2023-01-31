d531d85cad1f3a3127a5be932a1a1447fe22db70
Dalton Walls <scallywig@gmail.com>
2023-01-30 22:05:56 -0500

    Update Encapsulate

--- Encapsulate.py
+++ Encapsulate.py
@@ -1,15 +1,23 @@
 class Cat: 
-    def __init__(self, pet):
-        self.pet = pet
-        self.__age = 10 # Private attributes/methods
+    def __init__(self, name, age, breed, gender):
+        self.__name = name
+        self.__age = age
+        self._breed = breed
+        self._gender = gender
 
 
-    def print_age(self):
-        print(self.age)
+    def setName(self, private):
+        self.__name = private
+    
+    def getName(self):
+        print(self.__name)
 
-x = Cat('furry') # Defining class outside of the function
-print(x.__dict__) # using dictionary method to show results of redefined class
-print(x._Cat__age)
-x._Cat__age = 3  # This shows that encapsulations can be changed with new result
-print(x.__dict__)
-print(x._Cat__age+7) # This shows that operators can be used to change private attributes as well 
+
+p1 = Cat("Shimmy", 5, 'bengal', 'F')
+print(p1._breed) # This line and 20 are protected
+
+p1._breed = "m"
+print(p1._breed)
+p1.getName() # This line is accessing the privte property
+p1.setName("Laura")
+p1.getName()
