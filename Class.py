abc21aad509df61e5bf1b87bdfa60c04f826ffce
Dalton Walls <scallywig@gmail.com>
2024-02-28 16:11:00 -0500

    edit

--- Class.py
+++ Class.py
@@ -5,8 +5,15 @@ class name:
         self.lastName = lastName
     def printName(self):
         print("Hello, I am {0} {1}".format(self,firstName,lastName))
+        
+def this(backwards):
+    return backwards[::-1]
+    
+this_chat = this('I love spending time with family on all major holidays!')
+
+print(this_chat)
 
 
-#Passing in t he actual object values
+#Passing in the actual object values
 person=name('Air', 'Jordan')
 person.printName()
